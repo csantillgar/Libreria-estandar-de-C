78f64cdd730e4972c770e7d553a98472f6bd6ea2
csantillgar <csantill@myuax.com>
2023-12-18 21:54:03 +0100

    Ejercicio2a

---
+++ Ejercicio2/main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <cstring>
+
+int main(){
+    char str1[] = "Hola";
+    char str2[] = "Ruben";
+
+    int comparison = strcmp(str1, str2);
+    if (comparison==0) {
+        printf("Las cadenas son exactamente iguales\n");
+    }else {
+        printf("Las cadenas son distintas\n");
+    }
+
+    return 0;
+}
