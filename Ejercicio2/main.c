37526dd40e0006f8e1850c72053e26895f424571
csantillgar <csantill@myuax.com>
2023-12-18 22:01:50 +0100

    Ejercicio2b

---
+++ Ejercicio2/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char str[] = "Holaaaa";
+    int length = strlen(str);
+   printf("La longitud de '%s'es %d\n", str, length);
+    return 0;
+}
