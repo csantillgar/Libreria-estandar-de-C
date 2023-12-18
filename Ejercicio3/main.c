656d2245cdd90fc9d849e5f38d6ea99eafa864d3
csantillgar <csantill@myuax.com>
2023-12-18 23:11:43 +0100

    Ejercicio3a

--- Ejercicio3/main.c
+++ Ejercicio3/main.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 int main(){
-    FILE *file = fopen ("file.txt", "r");
+    FILE *file = fopen ("file.txt", "w");
     if ( file == NULL ) {
-        printf("Error: No se consiguio abrir el archivo\n");
+        printf("Error: No se consiguio abrir para escritura\n");
         return 1;
     }
+    fprintf(file, "Hola, profe\n");
     fclose(file);
+
+    file = fopen("file.txt", "r");
+    if ( file == NULL ) {
+        printf("Error: No se consiguio abrir para lectura\n");
+        return 1;
+    }
+    char buffer[50];
+    fscanf(file, "%s\n", buffer);
+    fclose(file);
+
+
     return 0;
 
 }
